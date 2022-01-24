0e3cbc22c2c93b72b8c333b540f1181734a2affd
Irtiza Hafiz <irtiza@irtiza.local>
2022-01-24 14:57:26 -0800

    working implementation

--- frontend/src/components/TransactionsTable.js
+++ frontend/src/components/TransactionsTable.js
@@ -1,45 +1,40 @@
 import React from "react";
 import {
-  Container,
-  Center,
   Text,
-  Heading,
   VStack,
-  HStack,
   Table,
   Thead,
   Tbody,
-  Tfoot,
   Tr,
   Th,
-  Td,
   TableCaption,
 } from "@chakra-ui/react";
-import { useState, useEffect } from "react";
 import TransactionItem from "./TransactionItem";
 
 export default function TransactionsTable({ transactions }) {
-  console.log("rendering table..");
-  console.log(transactions.length);
-  console.log(transactions[0]);
-
   return (
-    <Table size="sm" variant="striped" colorScheme="blackAlpha">
-      <TableCaption>All crypto buy and sell records</TableCaption>
-      <Thead>
-        <Tr>
-          <Th>Name</Th>
-          <Th>Symbol</Th>
-          <Th>Amount</Th>
-          <Th>Number of Coins</Th>
-          <Th>Date</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {transactions.map((tran) => {
-          return <TransactionItem transaction={tran}></TransactionItem>;
-        })}
-      </Tbody>
-    </Table>
+    <VStack>
+      <Text> Recent Transactions</Text>
+      <Table size="sm" variant="striped" colorScheme="blackAlpha" width={20}>
+        <TableCaption>All crypto buy and sell records</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>Name</Th>
+            <Th>Symbol</Th>
+            <Th>Amount</Th>
+            <Th>Number of Coins</Th>
+            <Th>Price Purchased At</Th>
+            <Th>Date</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {transactions.map((tran, index) => {
+            return (
+              <TransactionItem key={index} transaction={tran}></TransactionItem>
+            );
+          })}
+        </Tbody>
+      </Table>
+    </VStack>
   );
 }
